a3f4c84bc0bd90bee609a301f401f0b1f4260d71
Michał Szymański <smiszym@gmail.com>
2018-10-22 13:32:47 +0200

    [upstreamed] Give more information when raising exception

--- lcs/utils.py
+++ lcs/utils.py
@@ -3,7 +3,8 @@ from typing import Callable
 
 def check_types(oktypes, o):
     if not isinstance(o, oktypes):
-        raise TypeError("Wrong element type", o)
+        raise TypeError(
+            "Wrong element type: object {}, type {}".format(o, type(o)))
 
 
 def parse_state(raw_state, perception_mapper_fcn: Callable=None):
