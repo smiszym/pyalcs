c1ee8462878e436243fbb5d9859f0330d01c2174
Michał Szymański <smiszym@gmail.com>
2018-10-29 17:50:37 +0100

    Fix typo: is 'intermediate reward', should be 'immediate reward'

--- lcs/agents/racs/Classifier.py
+++ lcs/agents/racs/Classifier.py
@@ -14,7 +14,7 @@ class Classifier:
                  effect: Optional[Effect] = None,
                  quality: float = 0.5,
                  reward: float = 0.5,
-                 intermediate_reward: float = 0.0,
+                 immediate_reward: float = 0.0,
                  numerosity: int = 1,
                  experience: int = 1,
                  talp=None,
@@ -46,7 +46,7 @@ class Classifier:
         self.mark = Mark(cfg=cfg)
         self.q = quality
         self.r = reward
-        self.ir = intermediate_reward
+        self.ir = immediate_reward
         self.num = numerosity
 
         self.exp = experience
@@ -90,7 +90,7 @@ class Classifier:
             effect=old_cls.effect,
             quality=old_cls.q,
             reward=old_cls.r,
-            intermediate_reward=old_cls.ir,
+            immediate_reward=old_cls.ir,
             cfg=old_cls.cfg)
 
         new_cls.tga = time
