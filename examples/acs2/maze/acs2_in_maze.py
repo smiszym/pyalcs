1754701cd839dd90c0c02e117ca329a9cf736d29
Michał Szymański <smiszym@gmail.com>
2018-11-05 15:20:57 +0100

    [upstreamed] Add argument parsing to ACS2 in maze example

--- examples/acs2/maze/acs2_in_maze.py
+++ examples/acs2/maze/acs2_in_maze.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 import gym
@@ -14,24 +15,31 @@ logger = logging.getLogger("integration")
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--environment", default="BMaze4-v0")
+    parser.add_argument("--epsilon", default=1.0, type=float)
+    parser.add_argument("--ga", action="store_true")
+    parser.add_argument("--explore-trials", default=50, type=int)
+    parser.add_argument("--exploit-trials", default=10, type=int)
+    args = parser.parse_args()
 
     # Load desired environment
-    maze = gym.make('BMaze4-v0')
+    maze = gym.make(args.environment)
 
     # Configure and create the agent
     cfg = Configuration(8, 8,
-                        epsilon=1.0,
-                        do_ga=False,
+                        epsilon=args.epsilon,
+                        do_ga=args.ga,
                         performance_fcn=calculate_performance)
     logger.info(cfg)
 
     # Explore the environment
     agent = ACS2(cfg)
-    population, explore_metrics = agent.explore(maze, 50)
+    population, explore_metrics = agent.explore(maze, args.explore_trials)
 
     # Exploit the environment
     agent = ACS2(cfg, population)
-    population, exploit_metric = agent.exploit(maze, 10)
+    population, exploit_metric = agent.exploit(maze, args.exploit_trials)
 
     for metric in exploit_metric:
         logger.info(metric)
